ae2724b3b03110ff3ea9f6aa0416e7a8d4948751
Martin <damien.martin@epfl.ch>
2018-04-19 13:08:28 +0200

    Add Parameter class to store param and gradient, Linear.backward working

--- simplenet/modules.py
+++ simplenet/modules.py
@@ -2,12 +2,24 @@ import torch
 import numpy as np
 
 
+class Parameter(object):
+    def __init__(self, init_tensor):
+        self.data = init_tensor
+        self.grad = torch.zeros(init_tensor.shape)
+
+    def zero_grad(self):
+        self.grad.zero_()
+
+    def add_grad(self, grad_tensor):
+        self.grad.add_(grad_tensor)
+
+
 class Module(object):
     def __init__(self):
         super(Module, self).__init__()
         self.params = None
-        self.gradients = None
         self.activations = None
+        self.outputs = None
 
     def __repr__(self):
         return self.__class__.__name__ + "()"
@@ -15,7 +27,7 @@ class Module(object):
     def get_params(self):
         return self.params
 
-    def forward(self , *input):
+    def forward(self, *input):
         raise NotImplementedError
 
     def backward(self, *gradwrtoutput):
@@ -27,12 +39,12 @@ class ReLU(Module):
         super(ReLU, self).__init__()
 
     def forward(self, input):
-        self.activations = input
         output = input.clamp(0, np.inf)
+        self.outputs = output
         return output
 
     def backward(self, gradwrtoutput):
-        g_output = (gradwrtoutput * (0.5 + 0.5*self.activations.sign())).sum(0)
+        g_output = (gradwrtoutput * self.outputs.sign()).sum(0)
         return g_output
 
 
@@ -41,33 +53,33 @@ class Tanh(Module):
         super(Tanh, self).__init__()
 
     def forward(self, input):
-        self.activations = input
-        output = input.tanh()
-        return output
+        self.outputs = input.tanh()
+        return self.outputs
 
     def backward(self, gradwrtoutput):
-        g_output = (gradwrtoutput * (1 - self.activations.tanh()**2)).sum(0)
+        g_output = (gradwrtoutput * (1 - self.outputs**2)).sum(0)
         return g_output
 
 
 class Linear(Module):
-    def __init__(self, input_dim, output_dim, weights_init='normal'):
+    def __init__(self, input_dim, output_dim, w_init=None, b_init=None):
         super(Linear, self).__init__()
         self.nb_units = output_dim
         self.input_dims = input_dim
-        self.params = [torch.FloatTensor(output_dim, input_dim).normal_(), torch.FloatTensor(output_dim).zero_()]
-        self.gradients = list([torch.FloatTensor(p.size()).zero_() for p in self.params])
+        self.params = {
+            'w': Parameter(torch.randn(output_dim, input_dim) if w_init is None else w_init),
+            'b': Parameter(torch.zeros(output_dim) if b_init is None else b_init)
+        }
 
     def forward(self, input):
         self.activations = input
-        return (input @ self.params[0].t()) + self.params[1]
+        return (input @ self.params['w'].data.t()) + self.params['b'].data
 
     def backward(self, gradwrtoutput):
-        self.gradients[1] += gradwrtoutput.sum(0)
-        g = gradwrtoutput.sum()*self.activations
-        self.gradients[0] += g.sum(0)
-        g_output = gradwrtoutput*self.params[0]
-        return g_output
+        self.params['b'].add_grad(gradwrtoutput.sum(0))
+        self.params['w'].add_grad(self.activations.t()@gradwrtoutput)
+        g_output = gradwrtoutput@self.params['w'].data
+        return g_output.sum(0)
 
     def __repr__(self):
         return "Linear({})".format(str(self.nb_units))
--- tests/module_tests.py
+++ tests/module_tests.py
@@ -9,10 +9,10 @@ from torch.autograd.variable import Variable
 class ModulesTest(unittest.TestCase):
 
     def setUp(self):
-        n = 4
-        self.input_dims = 3
-        self.input = torch.randn(n, self.input_dims)
-        self.gradwrtout = torch.randn(n, self.input_dims)
+        self.n = 10000
+        self.input_dims = 100
+        self.input = torch.randn(self.n, self.input_dims)
+        self.gradwrtout = torch.randn(self.n, self.input_dims)
 
         self.addTypeEqualityFunc(torch.FloatTensor, self._tensor_equal)
 
@@ -20,7 +20,7 @@ class ModulesTest(unittest.TestCase):
     def torch_eval_f_backward(self, f, gradwrtout):
         tin = Variable(self.input, requires_grad=True)
         tout = f(tin)
-        tout.backward(self.gradwrtout)
+        tout.backward(gradwrtout)
         grad = tin.grad.data.sum(0)
         return grad
 
@@ -29,12 +29,12 @@ class ModulesTest(unittest.TestCase):
 
         model = modules.ReLU()
         out = model.forward(self.input)
-        assert(torch.equal(model.activations, self.input))
+        assert(torch.equal(model.outputs, out))
         assert(torch.equal(out, tout.data))
 
     def test_relu_backward(self):
         relu = modules.ReLU()
-        relu.activations = self.input
+        relu.forward(self.input)
         grad = relu.backward(self.gradwrtout)
         tgrad = self.torch_eval_f_backward(F.relu, self.gradwrtout)
         self.assertEqual(grad, tgrad)
@@ -45,12 +45,12 @@ class ModulesTest(unittest.TestCase):
 
         model = modules.Tanh()
         out = model.forward(self.input)
-        assert(torch.equal(model.activations, self.input))
+        assert(torch.equal(model.outputs, out))
         assert(torch.equal(out, tout))
 
     def test_tanh_backward(self):
         tanh = modules.Tanh()
-        tanh.activations = self.input
+        tanh.forward(self.input)
         grad = tanh.backward(self.gradwrtout)
         tgrad = self.torch_eval_f_backward(F.tanh, self.gradwrtout)
         self.assertEqual(grad, tgrad)
@@ -62,29 +62,32 @@ class ModulesTest(unittest.TestCase):
         tparams = tuple(p.data.clone() for p in tmodel.parameters())
         tout = tmodel.forward(torch.autograd.Variable(self.input))
 
-        model = modules.Linear(*dims)
-        model.params = tparams
+        model = modules.Linear(*dims, w_init=tparams[0], b_init=tparams[1])
         out = model.forward(self.input)
         assert(torch.equal(model.activations, self.input))
         assert(torch.equal(out, tout.data))
 
     def test_linear_backward(self):
-        gradwrtout = torch.randn(4, 10)
+        gradwrtout = torch.randn(self.n, 10)
         weights = torch.randn(10, self.input_dims)
         bias = torch.randn(10)
-        model = modules.Linear(self.input_dims, 10)
-        model.params = (bias, weights)
+        model = modules.Linear(self.input_dims, 10, w_init=weights, b_init=bias)
         model.activations = self.input
         gradwrtin = model.backward(gradwrtout)
-        lin_f = lambda input: F.linear(input, Variable(weights, requires_grad=True), Variable(bias, requires_grad=True))
+
+        ws = Variable(weights, requires_grad=True)
+        bs = Variable(bias, requires_grad=True)
+        lin_f = lambda input: F.linear(input, ws, bs)
         tgrad = self.torch_eval_f_backward(lin_f, gradwrtout)
         self.assertEqual(gradwrtin, tgrad)
 
     def _tensor_equal(self, x, y, msg):
         def round(x):
-            return (x*1e6).round()
+            return (x*100).round() #FIXME ????
 
         if not torch.equal(x, y):
             if not torch.equal(round(x), round(y)):
+                for i in range(x.shape[0]):
+                   print(x[i], y[i])
                 m = "x!=y" if msg is None else msg
                 raise self.failureException(m)
